aaed470e39e40fd7c38303a2b8026395237972b1
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2022-01-20 20:29:45 +0900

    Update Adafruit_NeoPixel_ver.ino

--- Adafruit_NeoPixel_ver/Adafruit_NeoPixel_ver.ino
+++ Adafruit_NeoPixel_ver/Adafruit_NeoPixel_ver.ino
@@ -1,22 +1,14 @@
 #include "LedController.h"
-#include "WaveGenerator.h"
-#include "VariableController.h"
 LedController led0(2, 20, 50);
 LedController led1(3, 20, 50);
 LedController led2(4, 20, 50);
 
-WaveGenerator sin8bit(5000, 1000);
-
 void setup() {
   Serial.begin(115200);
-  led0.setRBrightness(100);
 }
 
 void loop() {
-  sin8bit.Update();
   led0.flowing(500, 5, 150, 0xff00ff, 0x00ff00);
   led1.rainbow(sin8bit.GetTriangle(0, -500), 20);
   processingus("\t\n");
 }
-//13,100,50,1000,20:2380us
-//->2092us
