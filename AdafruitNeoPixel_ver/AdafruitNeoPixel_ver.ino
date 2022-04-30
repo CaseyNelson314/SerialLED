d95d719265e88702740ae74d2f2a1950d906c398
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2022-05-01 02:06:02 +0900

    Add files via upload

--- AdafruitNeoPixel_ver/AdafruitNeoPixel_ver.ino
+++ AdafruitNeoPixel_ver/AdafruitNeoPixel_ver.ino
@@ -1,23 +1,23 @@
-#define ADDRESS2 2
-#define ADDRESS3 3
-#define ADDRESS4 4
+#include "SerialLedController.h"
 
-#define LED_NUM 20
+SerialLedController pixel(4, 100);
 
-#define BRIGHTNESS 50
-
-#include "LedController.h"
-
-LedController led0(ADDRESS2, LED_NUM, BRIGHTNESS);
-LedController led1(ADDRESS3, LED_NUM, BRIGHTNESS);
-LedController led2(ADDRESS4, LED_NUM, BRIGHTNESS);
+void processingus(String code = "");
 
 void setup() {
   Serial.begin(115200);
+  pixel.setRBrightness(200);
+  pixel.setGBrightness(200);
 }
 
 void loop() {
-  led0.flowing(300,  5, 150, 0xffffff, 0xff00ff);
-  led1.stopMotion(1000);
-  processingus("\t\n");
+  pixel.showRainbow(3000, 100);
+  processingus("\n");
+}
+
+void processingus(String code) {
+  static uint32_t holdTime;
+  Serial.print(micros() - holdTime);
+  Serial.print(code);
+  holdTime = micros();
 }
